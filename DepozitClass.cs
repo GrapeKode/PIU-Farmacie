3e74083e910cca5e32bcd45c4a52cb56a6e19d2a
Grapekode <ruben_information@yahoo.ro>
2020-05-28 20:47:30 +0300

    Tema Farmacie - Consola

---
+++ DepozitClass.cs
@@ -0,0 +1,144 @@
+﻿using Medicament;
+using System;
+
+namespace Farmacie
+{
+    public class Depozit
+    {
+        // date membre private
+        Medicine[] medicamente; // Medicamente
+        string[] nume; // Indexare dupa nume
+        int[] stoc;       // int[] stoc; // Numarul de produse disponibile
+        int index;
+
+        // Constructors
+        public Depozit()
+        {
+            index = 0;
+            nume = new string[100];
+            nume[0] = "unknown";
+            stoc = new int[100];
+            stoc[0] = 0; 
+            medicamente = new Medicine[100];
+        }
+        public Depozit(Medicine Medicament, int _stoc = 1)
+        {
+            medicamente = new Medicine[100];
+            medicamente[0] = Medicament;
+            nume = new string[100];
+            nume[0] = Medicament.getNume().ToLower();
+            stoc[0] = _stoc;
+            index = 1;
+        }
+
+        // Setters
+        public void setMedicament(Medicine Medicament, int _stoc = 1)
+        {
+            medicamente[index] = Medicament;
+            nume[index] = Medicament.getNume().ToLower();
+            stoc[index] = _stoc;
+            index += 1;
+        }
+        public void editMedicament(Medicine Medicament, int _stoc = 1) 
+        {
+            string _nume = Medicament.getNume();
+            int editIndex = getIndexMedicament(_nume);
+            if (editIndex == -1)
+            {
+                Console.WriteLine("Medicamentul nu a fost gasit.");
+            }
+            else
+            {
+                medicamente[editIndex] = Medicament;
+                nume[editIndex] = _nume.ToLower();
+                stoc[editIndex] = _stoc;
+                Console.WriteLine("Medicamentul [" + _nume + "] a fost editat cu succes.");
+            }
+        }
+        public void removeMedicament(string name) {
+            int indexMedicament = getIndexMedicament(name);
+
+            if (isEmptyDepozit()) {
+                Console.WriteLine("Depozitul este gol.");
+                return;
+            }
+            if (indexMedicament == -1) {
+                Console.WriteLine("Medicamentul nu a fost gasit.");
+                return;
+            }
+
+            for (int i = 0; i <= index; i++) {
+                if (i > indexMedicament) {
+                    medicamente[i-1] = medicamente[i];
+                }
+            }
+
+            nume[index] = "unknown";
+            index--;
+
+            Console.WriteLine("Medicamentul a fost sters cu succes.");
+        }
+
+        // Getters
+        public string getMedicamente()
+        {
+            string result = "\n";
+
+            if (isEmptyDepozit())
+            {
+                return "Depozitul este gol.";
+            }
+
+            for (int i = 0; i < index; i++)
+            {
+                result += medicamente[i].ConvertToString() + (i + 1 != index ? "\n\n---------------------------------------------------------\n\n" : "\n\n");
+            }
+
+            return result;
+        }
+        public string getNumeMedicamente()
+        {
+            string result = "";
+            if (isEmptyDepozit())
+            {
+                return "Depozitul este gol.";
+            }
+            for (int i = 0; i < index; i++)
+            {
+                result += nume[i] + (i + 1 < index ? ", " : "");
+            }
+            return result;
+        }
+        public int getTotalMedicamente() { return index; }
+        public Medicine findMedicament(string _nume) 
+        {
+            int findIndex = getIndexMedicament(_nume);
+            if (findIndex == -1)
+            {
+                return new Medicine();
+            }
+
+            return medicamente[findIndex];
+        }
+        public bool existsMedicament(string _nume) { return getIndexMedicament(_nume) > -1; }
+
+        // Validations
+        private bool isEmptyArray(string[] _arr) { return _arr.GetLength(0) > 0; }
+        private int getIndexMedicament(string name) {
+            if (index == 0) {
+                return -1;
+            }
+            
+            for (int i = 0; i < index; i++) {
+                if (medicamente[i].getNume().ToLower() == name.ToLower()) {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        // Utils
+        public bool isEmptyDepozit() { return index == 0; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,391 @@
+﻿using Farmacie;
+using Medicament;
+using System;
+using System.Collections.Specialized;
+
+namespace Tema_6
+{//
+    class MainClass
+    {
+        static void addNume(Medicine M)
+        {
+            string nume; // Numele
+            Console.Write("Introduceti numele medicamentului: ");
+            nume = Console.ReadLine();
+            M.setNume(nume);
+        }
+        static void addGramaj(Medicine M)
+        {
+            int gramaj; // Gramaj
+            Console.Write("Introduceti gramajul medicamentului: ");
+            gramaj = Int32.Parse(Console.ReadLine());
+            M.setGramaj(gramaj);
+        }
+        static void addInterval(Medicine M)
+        {
+            int interval; // Intervalul orar
+            Console.Write("Intervalul orar de administrare: ");
+            interval = Int32.Parse(Console.ReadLine());
+            M.setInterval(interval);
+        }
+        static void addPret(Medicine M)
+        {
+            double pret; // Pret
+            Console.Write("Pret: ");
+            pret = Double.Parse(Console.ReadLine());
+            M.setPret(pret);
+        }
+        static void addValabilitate(Medicine M)
+        {
+            string valabilitate; // Data expirarii
+            Console.Write("Termenul de valabilitate (15-0-1997): ");
+            valabilitate = Console.ReadLine();
+            M.setValabilitate(valabilitate);
+        }
+        static void addTinta(Medicine M)
+        {
+            string tinta; // copii, adulti
+            Console.Write("Tinta (copii | adulti): ");
+            tinta = Console.ReadLine();
+            M.setTinta(tinta);
+        }
+        static void addScop(Medicine M)
+        {
+            int scopLen = -1;
+            string[] scop = new string[5]; // Ameliorare durere
+            Console.WriteLine("Beneficii:");
+            Console.Write("Introduceti numarul de beneficii (0 - 5):");
+            while (scopLen == -1)
+            {
+                scopLen = Int32.Parse(Console.ReadLine());
+                if (scopLen > 5 || scopLen < 0)
+                {
+                    Console.Write("Introduceti un numar cuprins intre 0 si 5 inclusiv: ");
+                    scopLen = -1;
+                }
+            }
+            Console.WriteLine("Introduceti beneficiile aduse");
+            for (int i = 0; i < scopLen; i++)
+            {
+                Console.Write("Beneficiul[" + (i + 1) + "]: ");
+                scop[i] = Console.ReadLine();
+            }
+            M.setScop(scop, scopLen);
+        }
+
+        // Main functions
+        static Medicine addMedicament(Medicine M)
+        {
+            M = new Medicine();
+
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 1. Creare medicament\n");
+
+            // Nume
+            addNume(M);
+            // Gramaj
+            addGramaj(M);
+            // Interval
+            addInterval(M);
+            // Pret
+            addPret(M);
+            // Valabilitate
+            addValabilitate(M);
+            // Tinta
+            addTinta(M);
+            // Scop
+            addScop(M);
+
+            Console.WriteLine("Medicament adaugat cu succes.");
+            return M;
+        }
+        public static void printMedicament(Medicine M)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 2. Afisare medicament\n");
+
+            Console.WriteLine(M.ConvertToString());
+        }
+        public static void editMedicament(Medicine M)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 3. Editare medicament\n");
+            System.Console.WriteLine("INFO: Modificarea i se va face si medicamentului din depozit.\n\n");
+
+            Console.WriteLine(
+                "1. Editeaza numele\n" +
+                "2. Editeaza gramajul\n" +
+                "3. Editeaza intervalul orar de administrare\n" +
+                "4. Editeaza pretul\n" +
+                "5. Editeaza termenul de valabilitate\n" +
+                "6. Editeaza tinta (copii/adulti)\n" + 
+                "7. Editeaza scopul\n" +
+                "X. Ierire din modul editare\n\n" +
+                "Alegeti optiunea: "
+            );
+            string opt = Console.ReadLine();
+
+            switch(opt.ToUpper())
+            {
+                case "1":
+                    addNume(M);
+                    break;
+                case "2":
+                    addGramaj(M);
+                    break;
+                case "3":
+                    addInterval(M);
+                    break;
+                case "4":
+                    addPret(M);
+                    break;
+                case "5":
+                    addValabilitate(M);
+                    break;
+                case "6":
+                    addTinta(M);
+                    break;
+                case "7":
+                    addScop(M);
+                    break;
+                case "QUIT":
+                case "EXIT":
+                case "X":
+                    return;
+                default:
+                    Console.WriteLine("Optiune invalida!");
+                    break;
+            }
+
+            editMedicament(M);
+        }
+        static void comparare(Depozit D)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 4. Comparare medicamente");
+            Console.WriteLine("INFO: Aceasta optiune este valabila doar pentru medicamentele aflate in depozit.\n\n");
+
+            if (D.isEmptyDepozit())
+            {
+                Console.WriteLine("Depozitul este gol. Adaugati medicamente mai intai.");
+                return;
+            }
+
+            Console.WriteLine("In depozit sunt (" + D.getTotalMedicamente() + ") medicamente:\n[" + D.getNumeMedicamente() + "]");
+            Console.WriteLine("\nIntroduceti numele celor doua medicamente: ");
+
+            string nume1 = Console.ReadLine(), nume2 = Console.ReadLine();
+
+            if (!D.existsMedicament(nume1))
+            {
+                Console.WriteLine("Medicamentul `" + nume1 + "` nu exista");
+            }
+            if (!D.existsMedicament(nume2))
+            {
+                Console.WriteLine("Medicamentul `" + nume2 + "` nu exista");
+            }
+
+            Console.WriteLine("Medicamentele alese: `" + nume1 + "` si `" + nume2 + "`");
+
+            Medicine M1 = new Medicine();
+            Medicine M2 = new Medicine();
+
+            M1 = D.findMedicament(nume1);
+            M2 = D.findMedicament(nume2);
+
+            Console.WriteLine("Comparatie:");
+            Console.WriteLine(M1.Comparare(M2));
+        }
+        public static void addDepozit(Depozit D, Medicine M)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 5. Adaugare in depozit\n");
+
+            if (!M.isValidMedicament())
+            {
+                Console.WriteLine("Creati un medicament inainte de-al depozita.");
+                return;
+            }
+
+            D.setMedicament(M);
+
+            if (!D.existsMedicament(M.getNume()))
+            {
+                Console.WriteLine("Oh... Ceva n-a mers bine.");
+                return;
+            }
+            Console.WriteLine("Medicamentul `" + M.getNume() + "` a fost adaugat cu succes.");
+        }
+
+        public static void printDepozit(Depozit D)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 6. Afisare medicamentele din depozit\n");
+
+            Console.WriteLine(D.getMedicamente());
+        }
+
+        public static void editDepozit(Depozit D, Medicine M)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 7. Editare medicament din depozit\n");
+
+            if (D.isEmptyDepozit()) {
+                Console.WriteLine("Depozitul este gol.");
+                return;
+            }
+            if (M.isValidMedicament() && !D.existsMedicament(M.getNume())) {
+                Console.WriteLine("Aveti un medicament nesalvat. Doriti sa continuati? (y/N): ");
+                string moveOn = Console.ReadLine();
+
+                switch(moveOn.ToUpper()) {
+                    case "DA":
+                    case "D":
+                    case "YES":
+                    case "Y":
+                        break;
+                    default:
+                        return;
+                }
+            }
+
+            Console.WriteLine(
+                "Medicamente disponibile [" + D.getNumeMedicamente() + "]\n" +
+                "Introduceti numele medicamentului pentru editare: "
+            );
+            string name = Console.ReadLine();
+
+            if (!D.existsMedicament(name)) {
+                Console.WriteLine("Medicamentul {0} nu a fost gasit.", name);
+                return;
+            }
+
+            Medicine editM = new Medicine();
+            editM = D.findMedicament(name);
+            editMedicament(editM);
+            D.editMedicament(editM);
+            Console.WriteLine("Editat -----------------------------------------\n{0}", editM.ConvertToString());
+        }
+
+        public static void findDepozit(Depozit D)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 8. Cautare medicament in depozit\n");
+
+            Console.WriteLine("Introduceti numele medicamentului: ");
+            string name = Console.ReadLine();
+
+            Medicine M = D.findMedicament(name);
+
+            if (!D.existsMedicament(M.getNume())) {
+                System.Console.WriteLine("Medicamentul nu a fost gasit.");
+                return;
+            }
+
+            System.Console.WriteLine("Medicamentul a fost gasit:\n\n" + M.ConvertToString());
+        }
+
+        public static void removeDepozit(Depozit D)
+        {
+            Console.Clear();
+            Console.WriteLine("Optiunea aleasa: 9. Stergere medicament din depozit\n");
+
+            System.Console.WriteLine("Introduceti numele medicamentului: ");
+            string name = System.Console.ReadLine();
+
+            D.removeMedicament(name);
+        }
+
+        static void Main(string[] args)
+        {
+            // Depozit + Medicament
+            Depozit D = new Depozit();
+            Medicine M = new Medicine();
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Alegeti optiune (1-9):");
+                Console.Write(
+                  "1. Creare medicament;                 \n" +
+                  "2. Afisare medicament;                \n" +
+                  "3. Editare medicament;                \n" +
+                  "4. Comparare medicamente;             \n" +
+                  "5. Adaugare in depozit;               \n" +
+                  "6. Afisare medicamentele din depozit; \n" +
+                  "7. Editare medicament din depozit;    \n" +
+                  "8. Cautare medicament in depozit      \n" +
+                  "9. Stergere medicament din depozit    \n" +
+                  "x. Exit                               \n"
+                );
+
+                Console.Write("\nAlegeti optiunea: ");
+
+                switch (Console.ReadLine().ToLower())
+                {
+                    case "1":
+                        // Console.Clear();
+                        M = addMedicament(M);
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        // Console.Clear();
+                        Console.WriteLine("ENTERED INTO PRINT_MEDICAMENT");
+                        printMedicament(M);
+                        Console.ReadLine();
+                        break;
+                    case "3":
+                        // Console.Clear();
+                        editMedicament(M);
+                        Console.ReadLine();
+                        break;
+                    case "4":
+                        // Console.Clear();
+                        comparare(D);
+                        Console.ReadLine();
+                        break;
+                    case "5":
+                        // Console.Clear();
+                        addDepozit(D, M);
+                        Console.ReadLine();
+                        break;
+                    case "6":
+                        // Console.Clear();
+                        printDepozit(D);
+                        Console.ReadLine();
+                        break;
+                    case "7":
+                        // Console.Clear();
+                        editDepozit(D, M);
+                        Console.ReadLine();
+                        break;
+                    case "8":
+                        // Console.Clear();
+                        findDepozit(D);
+                        Console.ReadLine();
+                        break;
+                    case "9":
+                        // Console.Clear();
+                        removeDepozit(D);
+                        Console.ReadLine();
+                        break;
+                    case "exit":
+                    case "stop":
+                    case "x":
+                        Console.Clear();
+                        Console.WriteLine("Programul este pe cale sa se inchida; aceasta actiune va anula toate modificarile aduse. Continuati? (da | NU)");
+                        if (Console.ReadLine().ToLower() != "da")
+                        {
+                            break;
+                        }
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Comanda introdusa nu face parte din meniu.");
+                        Console.ReadLine();
+                        break;
+                }
+            }
+        }
+    }
+}
